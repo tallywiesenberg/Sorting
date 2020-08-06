e84b9ddd3a530b6638d7eede63fc5c2bcb63c5a1
Tally Wiesenberg <t.j.wies@gmail.com>
2020-08-05 23:32:01 -0400

    insert, selection, nearly bubble

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,3 +1,15 @@
+def insert_sort(nums):
+    for i in range(1, len(nums)):
+        temp = num[i]
+        j = i
+        while j > 0 and temp < num[j-i]:
+            num[j] = num[j-1]
+            j -= 1
+        num[j] = temp
+    
+    return nums
+
+
 # TO-DO: Complete the selection_sort() function below 
 def selection_sort( arr ):
     # loop through n-1 elements
@@ -6,11 +18,14 @@ def selection_sort( arr ):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-             
-
-
+        for j in range(smallest_index, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
 
         # TO-DO: swap
+        temp = arr[cur_index]
+        arr[cur_index] = arr[smallest_index]
+        arr[smallest_index] = temp
 
 
 
@@ -20,7 +35,14 @@ def selection_sort( arr ):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-
+    for i in range(0, len(arr) - 1):
+        swapped = True
+        while swapped:
+            if arr[i] > arr[i+1]:
+                temp = arr[i]
+                arr[i] = arr[i+1]
+                arr[i+1] = temp
+                swapped = False
     return arr
 
 
